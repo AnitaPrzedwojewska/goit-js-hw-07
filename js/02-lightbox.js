82f6c1f785eefeb3cbfeab32fc8090d0c6cf5f44
AnitaPrzedwojewska <nisiek@wp.pl>
2024-01-03 21:08:03 +0100

    Created second gallery - first step

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,12 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const galleryElement = document.querySelector(".gallery");
+const galleryList = galleryItems.map(({ preview, original, description }) =>
+  `<li>
+  <a class="gallery__item" href=${original}>
+  <img class="gallery__image" src=${preview} alt=${description} />
+</a></li>`
+).join("");
+
+galleryElement.insertAdjacentHTML("beforeend", galleryList);
